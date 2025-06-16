2cb61c730edb89845dfc543dd20746e370f19d9b
adithdotv <adithadiabd17@gmail.com>
2025-06-16 13:46:57 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <nav className='bg-blue-200 w-full align-middle p-10 justify-center flex'>
+          <h1 className='text-blue-600 text-4xl font-bold align-middle'>Voting Dapp</h1>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
